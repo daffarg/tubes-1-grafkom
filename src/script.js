159a856a7891d034e781b6d3de9d49f76595c033
afrizalsebastian <sebastiangurning@gmail.com>
2023-02-11 16:29:38 +0700

    feat : test polygon hover and not clean

--- src/script.js
+++ src/script.js
@@ -6,19 +6,18 @@ if(!gl){
 }
 
 const vertexTemp = []
+const colorTemp = [
+  0, 0, 0,    //V1.color
+  0, 1, 0,
+  0, 0, 1,
+  1, 0, 0,
+  1, 1, 1,
+  1, 0, 1
+]
+
 function handlePolygonButton(){
 
   canvas.onmousedown = (e) =>{
-
-    const colorTemp = [
-      0, 0, 0,    //V1.color
-      0, 1, 0,
-      0, 0, 1,
-      1, 0, 0,
-      1, 1, 1,
-      1, 0, 1
-    ]
-
     var clickX = e.clientX
     var clickY = e.clientY
     var rect = e.target.getBoundingClientRect();
@@ -45,9 +44,63 @@ function handlePolygonButton(){
     gl.shaderSource(vertexShader,vtx)
     gl.compileShader(vertexShader);
 
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-      console.error('Could not compile vertex shader: ' + gl.getShaderInfoLog(vertexShader));
+    //Fragment Shader
+    const ftx = document.getElementById('fragment-shader-2d').text
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+    gl.shaderSource(fragmentShader, ftx)
+    gl.compileShader(fragmentShader)
+
+    //Program
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    const positionLocation = gl.getAttribLocation(program, `a_position`);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0,0);
+
+    const colorLocation = gl.getAttribLocation(program, `a_color`);
+    gl.enableVertexAttribArray(colorLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+    gl.useProgram(program);
+    var count = vertexTemp.length/2
+    if(count >= 3){
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, count)
     }
+  }
+
+  
+  canvas.onmousemove = (e) => {
+    var clickX = e.clientX
+    var clickY = e.clientY
+    var rect = e.target.getBoundingClientRect();
+
+    var x,y;
+    x = (2*(clickX - rect.left) - canvas.width) / canvas.width;
+    y = (canvas.height - 2*(clickY - rect.top)) / canvas.height;
+    
+    if(vertexTemp.length >= 6){
+      vertexTemp[vertexTemp.length-2] = x
+      vertexTemp[vertexTemp.length-1] = y
+    }
+    // Testing for Polygon
+    const vertexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexTemp), gl.STATIC_DRAW)
+
+    const colorBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorTemp), gl.STATIC_DRAW)
+
+    //Vertex Shader
+    const vtx = document.getElementById('vertex-shader-2d').text
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER)
+    gl.shaderSource(vertexShader,vtx)
+    gl.compileShader(vertexShader);
 
     //Fragment Shader
     const ftx = document.getElementById('fragment-shader-2d').text
@@ -72,8 +125,10 @@ function handlePolygonButton(){
     gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
     gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, (6))
-  }
-  
+    var count = vertexTemp.length/2
+    if(count >= 2){
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, count)
+    }
+  }  
 }
 
